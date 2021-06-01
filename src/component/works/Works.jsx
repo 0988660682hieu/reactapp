fd096087f3599d1e3db1cb414a62dbd8ec381b15
0988660682hieu <80968463+0988660682hieu@users.noreply.github.com>
2021-06-01 20:44:03 +0700

    begin with wotks

--- src/component/works/Works.jsx
+++ src/component/works/Works.jsx
@@ -3,7 +3,17 @@ import "./works.scss";
 export default function Works() {
     return (
         <div class="works" id="works">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            
+                        </div>
+
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
